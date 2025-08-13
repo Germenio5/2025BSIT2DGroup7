d2bcaeb5adc54f390a33703689354fbbdcfc5d85
Germenio <39080535+Germenio5@users.noreply.github.com>
2025-08-13 12:18:22 +0800

    Add files via upload

---
+++ E-Kolekta/about_us.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <title>About us</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+    <?php require "views/header.php"; ?>
+    <div class="about_us">
+
+    </div>
+    <?php require "views/footer.php"; ?>
+</body>
+</html>
