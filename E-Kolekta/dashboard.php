d67f4baf7143337647d5ce8519a8ea986bfd654d
Germenio5 <germenio123@gmail.com>
2025-09-06 11:38:40 +0800

    Add Content In dashboard username and debug the responsivess in dashboard part

--- E-Kolekta/dashboard.php
+++ E-Kolekta/dashboard.php
@@ -11,6 +11,37 @@
         <?php include "views/sidebar.php"; ?>
 
         <div class="main-content">
+            <div id="username"> Hello, username!</div>
+
+            <div class="activity_status">
+                <div id="activity_status">Activity Status</div>
+                <div class="activity_status_container">
+                <div id="total_items_donated"> 
+                    <p> Total Items Donated </p> 
+                    <div id="total_items_donated_value"> 0 
+                        <div> items </div>
+                    </div>
+                </div>
+                <div id="total_e-waste_weight"> 
+                    <p> Total E-Waste Weight </p> 
+                    <div id="total_e-waste_weight_value"> 0 
+                        <div> kg </div>
+                    </div>
+                </div>
+                <div id="total_pickup"> 
+                    <p> Total Pickups Completed </p> 
+                    <div id="total_pickup_value"> 0 
+                        <div> times </div>
+                    </div>
+                </div>
+                <div id="total_drop-offs"> 
+                    <p> Total Drop-offs Completed </p> 
+                    <div id="total_drop-offs_value"> 0 
+                        <div> times </div>
+                    </div>
+                </div>
+                </div>
+            </div>
 
         </div>
     </div>
